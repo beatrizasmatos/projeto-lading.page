f1d3fab30cfbaa4ce3dc51e1242124547b4c4cd8
beatrizasmatos <ateliersophias@gmail.com>
2024-06-25 17:33:01 -0300

    Versão inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var setadireita = window.document.getElementById("setadireita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+function rolarparadireita() {
+    leonardo.style = "display:none;"
+    bruna.style = "display:flex;"
+    setaesquerda.style = "display:flex;"
+    setadireita.style = "display:none;"
+}
+
+function rolarparaesquerda() {
+    leonardo.style = "display:flex;"
+    bruna.style = "display:none;"
+    setaesquerda.style = "display:none;"
+    setadireita.style = "display:flex;"
+}
